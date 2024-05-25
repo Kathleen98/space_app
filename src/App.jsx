f58c092f6f426967fa122effeb7e83f39b8ab8b6
Kathleen Santos <kathleen.santos@csacademy.com.br>
2024-05-25 19:06:15 -0300

    feat: standardizing nomenclatures by adding Popular section

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import styled from "styled-components";
-import EstilosGlobais from "./components/EstilosGlobais";
-import Cabecalho from "./components/Cabecalho";
-import BarraLateral from "./components/BarraLateral";
-import Banner from "./components/Banner";
-import bannerBackground from '../public/img/banner.png';
-import Galeria from "./components/Galeria";
-import fotos from './fotos.json';
-import { useState } from "react";
-import ModalZoom from "./components/ModalZoom";
+import { styled } from "styled-components"
+import EstilosGlobais from "./componentes/EstilosGlobais"
+import Cabecalho from "./componentes/Cabecalho"
+import BarraLateral from "./componentes/BarraLateral"
+import Banner from "./componentes/Banner"
+import bannerBackground from './assets/banner.png'
+import Galeria from "./componentes/Galeria"
 
+import fotos from './fotos.json'
+import { useState } from "react"
+import ModalZoom from "./componentes/ModalZoom"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
   min-height: 100vh;
 `
+
 const AppContainer = styled.div`
-  width: 105.41vw;
-  max-width: 95%;
-  margin: 0px auto;
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
 `
-const Flex = styled.div`
+
+const MainContainer = styled.main`
   display: flex;
   gap: 24px;
 `
@@ -31,30 +33,33 @@ const ConteudoGaleria = styled.section`
   flex-grow: 1;
 `
 
-
-
 const App = () => {
-
   const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
-
+  const [fotoSelecionada, setFotoSelecionada] = useState(null)
+  
   return (
     <FundoGradiente>
       <EstilosGlobais />
       <AppContainer>
         <Cabecalho />
-        <Flex>
+        <MainContainer>
           <BarraLateral />
           <ConteudoGaleria>
-          <Banner 
-            backgroundImage={bannerBackground}
-            texto="A galeria mais completa de fotos do espaço!" 
-           />
-           <Galeria fotos={fotosDaGaleria} />
+            <Banner
+              texto="A galeria mais completa de fotos do espaço!"
+              backgroundImage={bannerBackground}
+            />
+            <Galeria 
+              aoFotoSelecionada={foto => setFotoSelecionada(foto)} 
+              fotos={fotosDaGaleria}
+            />
           </ConteudoGaleria>
-          
-        </Flex>
+        </MainContainer>
       </AppContainer>
-      <ModalZoom />
+      <ModalZoom 
+        foto={fotoSelecionada}
+        aoFechar={() => setFotoSelecionada(null)}
+      />
     </FundoGradiente>
   )
 }
--- src/components/Titulo/index.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Titulo = styled.h2`
-    color: #7B78E5;
-    font-size: 2em;
-    text-align: ${props => props.$alinhamento ? props.$alinhamento : 'left'};
-`
-
-export default Titulo
